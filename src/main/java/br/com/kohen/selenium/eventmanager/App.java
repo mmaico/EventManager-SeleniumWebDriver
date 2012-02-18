ee4854a9ccac6e0a8986cd5e8ae333e786a11fc6
Marcelo Maico <marcelomaicodejesus@MacBook-Pro.local>
2012-02-18 18:26:20 -0200

    Arquivos inciais do projeto de teste

---
+++ src/main/java/br/com/kohen/selenium/eventmanager/App.java
@@ -0,0 +1,13 @@
+package br.com.kohen.selenium.eventmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
